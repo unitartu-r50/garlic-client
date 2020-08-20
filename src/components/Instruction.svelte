73bd2c7dfd8a3f8423a267a670ace2b8dcc893d3
Ihar Suvorau <ihar.suvorau@gmail.com>
2020-08-20 13:30:20 +0300

    audio playing

--- src/components/Instruction.svelte
+++ src/components/Instruction.svelte
@@ -6,12 +6,27 @@
     function markVisited(id) {
         document.getElementById(id).style.background = "rgba(159, 241, 255, .15)";
     }
+
+    function playAudio(itemID) {
+        const audioElement = document.getElementById("audio-" + itemID);
+        if (audioElement) {
+            console.log("playing audio");
+            audioElement.play()
+                .catch(err => {
+                    console.error(err);
+                    notify("negative", err);
+                });
+        } else {
+            console.log("not playing audio");
+        }
+    }
 </script>
 
 {#if expanded}
     {#if item}
         <article id="{item.SayItem.ID}" class="instruction flex flex-column justify-between"
-                 on:click={markVisited(item.SayItem.ID)} on:click={sendInstruction(item.SayItem.ID)}>
+                 on:click={markVisited(item.SayItem.ID)} on:click={sendInstruction(item.SayItem.ID)}
+                 on:click={playAudio(item.SayItem.ID)}>
             <p class="h6 m0 mb4 bold caps">Question {index + 1}</p>
             <div>
                 <p class="h2 m0 mb1">{item.SayItem.Phrase}</p>
@@ -32,8 +47,10 @@
     {/if}
 {:else}
     {#if item}
-        <article id="{item.SayItem.ID}" class="instruction" on:click={markVisited(item.SayItem.ID)}
-                 on:click={sendInstruction(item.SayItem.ID)}>
+        <article id="{item.SayItem.ID}" class="instruction"
+                 on:click={markVisited(item.SayItem.ID)}
+                 on:click={sendInstruction(item.SayItem.ID)}
+                 on:click={playAudio(item.SayItem.ID)}>
             <p class="h2 m0 mb1">{item.SayItem.Phrase}</p>
             {#if item.SayItem.FilePath.length > 0}
                 <p class="h6 m0"><span class="bold">audio</span>: {item.SayItem.FilePath}</p>
@@ -45,6 +62,8 @@
                         <span class="bold">with delay</span>: {item.MoveItem.Delay / 1e9}s
                     {/if}
                 </p>
+            {/if}
+            {#if item.SayItem.FilePath.length > 0}
                 <audio id="audio-{item.SayItem.ID}" src="http://localhost:8080/{item.SayItem.FilePath}">
                     Your browser does not support the <code>audio</code> element.
                 </audio>
