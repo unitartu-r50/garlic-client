77e2f9c10108f363b0365d349c18000a179e8736
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-01-26 15:30:57 +0200

    Instruction refactored

--- src/components/Instruction.svelte
+++ src/components/Instruction.svelte
@@ -1,8 +1,15 @@
 <script>
     import {sendInstruction, notify} from './Helpers.svelte';
     import {serverIPStore} from './stores';
+    import InstructionFooter from "./InstructionFooter.svelte";
 
-    export let item, index = 0, expanded = false, name, small = false, clickTracking = true;
+    export let
+        item,
+        name,
+        index = 0,
+        expanded = false,
+        small = false,
+        clickTracking = true;
     let isMobile = false;
     let isSmallMobile = false;
 
@@ -45,25 +52,10 @@
         }
     }
 
-    function truncateLongString(value) {
-        // truncate on desktop, mobile has different layout, no need to truncate there
-        if (!isMobile) {
-            const maxAllowedChars = 40;
-            if (value.length > maxAllowedChars) {
-                return value.substring(0, maxAllowedChars) + "...";
-            }
-        }
-        return value;
-    }
+
 </script>
 
 <style>
-    .icon {
-        height: 1em;
-        margin-right: .25em;
-        margin-bottom: .35em;
-    }
-
     .instruction {
         display: flex;
         flex-direction: column;
@@ -75,16 +67,6 @@
             justify-content: normal;
         }
     }
-
-    .instruction-name {
-        font-size: 1.5rem;
-    }
-
-    @media (max-width: 600px) {
-        .instruction-name {
-            font-size: 1.25rem;
-        }
-    }
 </style>
 
 {#if expanded}
@@ -94,39 +76,7 @@
                  on:click={sendInstruction(item.ID, $serverIPStore)}
                  on:click={playAudio(item.SayItem.ID, item.SayItem.Delay)}>
             <p class="h6 m0 bold caps {isMobile ? 'mb2' : 'mb4'}">Question {index + 1}</p>
-            <div>
-                <p class="m0 mb1 instruction-name">{name}</p>
-                {#if item.SayItem.FilePath && item.SayItem.FilePath.length > 0}
-                    {#if item.SayItem.Delay > 0}
-                        <img class="icon" src="/images/speech-delay.svg" alt="audio with delay is present"
-                             title="audio with delay is present">
-                    {:else}
-                        <img class="icon" src="/images/speech.svg" alt="audio is present" title="audio is present">
-                    {/if}
-                {/if}
-                {#if item.MoveItem && (item.MoveItem.Name || item.MoveItem.FilePath)}
-                    {#if item.MoveItem.Delay > 0}
-                        <img class="icon" src="/images/pepper-icon-delay.svg" alt="movement with delay is present"
-                             title="movement with delay is present">
-                    {:else}
-                        <img class="icon" src="/images/pepper-icon.svg" alt="movement is present"
-                             title="movement is present">
-                    {/if}
-                {/if}
-                {#if item.ImageItem && item.ImageItem.FilePath}
-                    {#if item.ImageItem.Delay > 0}
-                        <img class="icon" src="/images/image-delay.svg" alt="delay is present"
-                             title="delay is present">
-                    {:else}
-                        <img class="icon" src="/images/image.svg" alt="no delay">
-                    {/if}
-                {/if}
-            </div>
-            {#if item.SayItem.FilePath.length > 0}
-                <audio id="audio-{item.SayItem.ID}" src="http://{$serverIPStore}:8080/{item.SayItem.FilePath}">
-                    Your browser does not support the <code>audio</code> element.
-                </audio>
-            {/if}
+            <InstructionFooter bind:item={item} {name} {expanded}/>
         </article>
     {/if}
 {:else}
@@ -135,47 +85,7 @@
                  on:click={markVisited}
                  on:click={sendInstruction(item.ID, $serverIPStore)}
                  on:click={playAudio(item.SayItem.ID, item.SayItem.Delay)}>
-            <p class="m0 mb1" class:instruction-name="{!small}">{truncateLongString(name)}</p>
-            <div>
-                {#if item.SayItem.FilePath && item.SayItem.FilePath.length > 0}
-                    {#if item.SayItem.Delay > 0}
-                        <img class="icon" src="/images/speech-delay.svg" alt="audio with delay is present"
-                             title="audio with delay is present">
-                    {:else}
-                        <img class="icon" src="/images/speech.svg" alt="audio is present" title="audio is present">
-                    {/if}
-                {/if}
-                {#if item.MoveItem && (item.MoveItem.Name || item.MoveItem.FilePath)}
-                    {#if item.MoveItem.Delay > 0}
-                        <img class="icon" src="/images/pepper-icon-delay.svg" alt="movement with delay is present"
-                             title="movement with delay is present">
-                    {:else}
-                        <img class="icon" src="/images/pepper-icon.svg" alt="movement is present"
-                             title="movement is present">
-                    {/if}
-                {/if}
-                {#if item.ImageItem && item.ImageItem.FilePath}
-                    {#if item.ImageItem.Delay > 0}
-                        <img class="icon" src="/images/image-delay.svg" alt="delay is present"
-                             title="delay is present">
-                    {:else}
-                        <img class="icon" src="/images/image.svg" alt="no delay">
-                    {/if}
-                {/if}
-                {#if item.URLItem && item.URLItem.URL.length > 0}
-                    {#if item.URLItem.Delay > 0}
-                        <img class="icon" src="/images/url-delay.svg" alt="URL with delay is present"
-                             title="URL with delay is present">
-                    {:else}
-                        <img class="icon" src="/images/url.svg" alt="URL is present" title="URL is present">
-                    {/if}
-                {/if}
-            </div>
-            {#if item.SayItem.FilePath.length > 0}
-                <audio id="audio-{item.SayItem.ID}" src="http://{$serverIPStore}:8080/{item.SayItem.FilePath}">
-                    Your browser does not support the <code>audio</code> element.
-                </audio>
-            {/if}
+            <InstructionFooter bind:item={item} {name} {small} {isMobile}/>
         </article>
     {/if}
 {/if}
