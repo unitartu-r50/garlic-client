14c7c6ca213ec38225b831808f4c0a24198fda87
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-01-27 19:12:01 +0200

    CollapsibleLibrary introduced

---
+++ src/components/CollapsibleLibrary.svelte
@@ -0,0 +1,38 @@
+<script>
+    import {slide} from 'svelte/transition';
+
+    export let title, isCollapsed = true;
+</script>
+
+<style>
+    .collapsible {
+        cursor: pointer;
+        transition: all .2s;
+    }
+
+    .collapsible:hover, .collapsible:active {
+        color: rgb(80, 228, 253);
+    }
+
+    .collapsible::before {
+        content: "–";
+        margin-right: .3em;
+        color: rgba(159, 241, 255, 1);
+    }
+
+    .collapsed::before {
+        content: "+";
+        margin-right: .2em;
+        color: rgba(159, 241, 255, 1);
+    }
+</style>
+
+<section class:mb4={!isCollapsed}>
+    <h2 class="h2 m0 mb1 collapsible" class:collapsed={isCollapsed}
+        on:click|preventDefault={() => {isCollapsed = !isCollapsed}}>{title}</h2>
+    {#if !isCollapsed}
+        <div transition:slide={{duration: 100}}>
+           <slot/>
+        </div>
+    {/if}
+</section>
