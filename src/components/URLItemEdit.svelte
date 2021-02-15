71d260217b93cc3b6bd9a3d42d75bc628f7d2954
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-02-15 17:50:13 +0200

    creating more general URLItemEdit component

---
+++ src/components/URLItemEdit.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let URLItem;
+</script>
+
+<style>
+    input[type=number] {
+        width: 5em;
+    }
+
+    label {
+        display: block;
+    }
+</style>
+
+{#if URLItem}
+    <section>
+        <h3 class="h4 m0 mb2">Show URL</h3>
+        <label class="mb1" for="{URLItem.ID}.URL">Web URL:
+            <input type="text" id="{URLItem.ID}.URL"
+                   bind:value={URLItem.URL}>
+        </label>
+        <label class="mb1" for="{URLItem.ID}.Delay">URL delay, s:
+            <input type="number" id="{URLItem.ID}.Delay"
+                   bind:value={URLItem.Delay}>
+        </label>
+    </section>
+{/if}
